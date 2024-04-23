7811057c7e39ad5ed48a6283dcea5101c4d9a28d
chanyagracet <cthanglerdsumpan@gmail.com>
2024-04-23 12:59:04 -0400

    create table queries

--- models/create_tables.js
+++ models/create_tables.js
@@ -22,69 +22,130 @@ async function create_tables(db) {
   // You'll need to define the names table.
   // var qa = db.create_tables('...');
 
-  // var qb = db.create_tables('CREATE TABLE IF NOT EXISTS titles ( \
-  //   tconst VARCHAR(10) PRIMARY KEY, \
-  //   titleType varchar(255), \
-  //   primaryTitle VARCHAR(255), \
-  //   originalTitle VARCHAR(255), \
-  //   startYear varchar(4), \
-  //   endYear varchar(4), \
-  //   genres VARCHAR(255) \
-  //   );')
-
-  //   var qc = db.create_tables('CREATE TABLE IF NOT EXISTS principals ( \
-  //     tconst VARCHAR(10), \
-  //     ordering int, \
-  //     nconst VARCHAR(10), \
-  //     category VARCHAR(255), \
-  //     job VARCHAR(255), \
-  //     characters VARCHAR(255), \
-  //     FOREIGN KEY (tconst) REFERENCES titles(tconst), \
-  //     FOREIGN KEY (nconst) REFERENCES names(nconst_short) \
-  //     );')
-
-  //   var qd = db.create_tables('CREATE TABLE IF NOT EXISTS recommendations ( \
-  //     person VARCHAR(10), \
-  //     recommendation VARCHAR(10), \
-  //     strength int, \
-  //     FOREIGN KEY (person) REFERENCES names(nconst_short), \
-  //     FOREIGN KEY (recommendation) REFERENCES names(nconst_short) \
-  //     );')
-  
-
-
-
-  // This table should already exist from HW3
-  var q1 = db.create_tables('CREATE TABLE IF NOT EXISTS friends ( \
-    followed VARCHAR(10), \
-    follower VARCHAR(10), \
-    FOREIGN KEY (follower) REFERENCES names(nconst), \
-    FOREIGN KEY (followed) REFERENCES names(nconst) \
+    var qrecs = db.create_tables('CREATE TABLE IF NOT EXISTS recommendations ( \
+      person VARCHAR(10), \
+      recommendation VARCHAR(10), \
+      strength int, \
+      FOREIGN KEY (person) REFERENCES users(users_id), \
+      FOREIGN KEY (recommendation) REFERENCES users(user_id) \
     );')
-
-    // TODO: create users table
-    var q2 = db.create_tables('CREATE TABLE IF NOT EXISTS users ( \
-      user_id INT AUTO_INCREMENT PRIMARY KEY,\
-      username VARCHAR(255) NOT NULL,\
-      hashed_password VARCHAR(255), \
-      linked_nconst VARCHAR(10), \
-      FOREIGN KEY (linked_nconst) REFERENCES names(nconst) \
-      );'
-    );
     
-    // TODO: create posts table
-    var q3 = db.create_tables('CREATE TABLE IF NOT EXISTS posts ( \
+    var qusers = db.create_tables('CREATE TABLE IF NOT EXISTS users ( \
+      user_id INT AUTO_INCREMENT PRIMARY KEY, \
+      username VARCHAR(255), \
+      firstname VARCHAR(255), \
+      lastname VARCHAR(255), \
+      affiliation VARCHAR(255), \
+      password int, \
+      birthday date, \
+      profile_photo BLOB, \
+    );')
+  
+    var qposts = db.create_tables('CREATE TABLE IF NOT EXISTS posts ( \
       post_id INT AUTO_INCREMENT PRIMARY KEY,\
-      parent_post INT,\
       title VARCHAR(255),\
-      content VARCHAR(255), \
+      content VARCHAR(500), \
       author_id INT, \
-      FOREIGN KEY (parent_post) REFERENCES posts(post_id), \
+      timestamp DATE, \
       FOREIGN KEY (author_id) REFERENCES users(user_id) \
       );'
-    );    
+    );
 
-    return await Promise.all([q1, q2, q3]);
+    // establishing the likes relationship
+    var qpostslikers = db.create_tables('CREATE TABLE IF NOT EXISTS posts_liked_by ( \
+      liker_id INT, \
+      post_id INT, \
+      FOREIGN KEY (liker_id) REFERENCES users(user_id), \
+      FOREIGN KEY (post_id) REFERENCES posts(post_id) \
+    );')
+
+    // establishing the likes comments relationship for post and commenter
+    var qcommenters = db.create_tables('CREATE TABLE IF NOT EXISTS comments_on_post_by ( \
+      comment_id INT, \
+      post_id INT, \
+      author_id INT, \
+      FOREIGN KEY (author_id) REFERENCES users(user_id), \
+      FOREIGN KEY (post_id) REFERENCES posts(post_id), \
+      FOREIGN KEY (comment_id) REFERENCES comments(comment_id) \
+    );')
+
+    // establishing the tagging relationship for post and tag(s)
+    var qposttags = db.create_tables('CREATE TABLE IF NOT EXISTS post_tagged_with ( \
+      post_id INT, \
+      hashtag_id INT, \
+      FOREIGN KEY (post_id) REFERENCES posts(post_id), \
+      FOREIGN KEY (hashtag_id) REFERENCES hashtags(hashtag_id) \
+    );')
+
+    // establishing the tagging relationship for user and tag(s)
+    var qusertags = db.create_tables('CREATE TABLE IF NOT EXISTS hashtag_by ( \
+      hashtag_id INT, \
+      user_id INT, \
+      FOREIGN KEY (user_id) REFERENCES users(user_id), \
+      FOREIGN KEY (hashtag_id) REFERENCES hashtags(hashtag_id) \
+    );')
+
+    /**** chat-related table ***/
+    var qchats = db.create_tables('CREATE TABLE IF NOT EXISTS chats ( \
+      chat_id INT AUTO_INCREMENT PRIMARY KEY, \
+      chatname VARCHAR(255), \
+    );')
+
+    // establishing the chat users in there
+    var quserchats = db.create_tables('CREATE TABLE IF NOT EXISTS user_chats ( \
+      user_id INT, \
+      chat_id INT, \
+      FOREIGN KEY (user_id) REFERENCES users(user_id), \
+      FOREIGN KEY (chat_id) REFERENCES chats(chat_id) \
+    );')
+
+    var qtexts = db.create_tables('CREATE TABLE IF NOT EXISTS texts ( \
+      text_id INT AUTO_INCREMENT PRIMARY KEY, \
+      author_id INT, \
+      chat_id INT, \
+      content VARCHAR(500), \
+      timestamp DATE, \
+      FOREIGN KEY (author_id) REFERENCES users(users_id), \
+      FOREIGN KEY (chat_id) REFERENCES chats(chat_id) \
+    );')
+
+    var qinvites = db.create_tables('CREATE TABLE IF NOT EXISTS invites ( \
+      invite_id INT AUTO_INCREMENT PRIMARY KEY, \
+      chat_id INT, \
+      invitee_id INT, \
+      inviter_id INT, \
+      confirmed BOOLEAN, \
+      FOREIGN KEY (invitee_id) REFERENCES users(users_id), \
+      FOREIGN KEY (inviter_id) REFERENCES users(users_id), \
+      FOREIGN KEY (chat_id) REFERENCES chats(chat_id) \
+    );')
+
+    var quserinvites = db.create_tables('CREATE TABLE IF NOT EXISTS user_invites ( \
+      user_id INT, \
+      invite_id INT, \
+      FOREIGN KEY (user_id) REFERENCES users(users_id) \
+      FOREIGN KEY (invite_id) REFERENCES invites(invite_id) \
+    );')
+
+    var qfriends = db.create_tables('CREATE TABLE IF NOT EXISTS friends ( \
+      followed INT, \
+      follower INT \
+      FOREIGN KEY (follower) REFERENCES users(user_id), \
+      FOREIGN KEY (followed) REFERENCES users(user_id) \
+      );')
+  
+    var qcomments = db.create_tables('CREATE TABLE IF NOT EXISTS comments ( \
+      comment_id INT AUTO_INCREMENT PRIMARY KEY, \
+      content VARCHAR(500),\
+      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, \
+      post_id INT, \
+      author_id INT, \
+      FOREIGN KEY (post_id) REFERENCES posts(post_id), \
+      FOREIGN KEY (author_id) REFERENCES users(user_id) \
+      ); '
+    );
+
+    return await Promise.all([qrecs, qusers, qposts, qpostslikers, qcommenters, qposttags, qusertags, qchats, quserchats, qtexts, qinvites, quserinvites, qfriends, qcomments]);
 }
 
 // Database connection setup

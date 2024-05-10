ad687752d07de94b54d903c5af39db5b0791fce3
iaincl <iain0523@sas.upenn.edu>
2024-05-10 22:06:03 +0000

    tried showing profile pic

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(cors({
 
 app.use(express.json()); 
 
+// app.use('/uploads', express.static('/nets2120/project-stream-team/uploads'));
+
 app.use(session({
   secret: 'nets2120_insecure', 
   saveUninitialized: true,
